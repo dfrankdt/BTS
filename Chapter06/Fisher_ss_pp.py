27eefb7d086353396bb7c0d33160eb1a56a1def7
Frank Lynch <lynch@ewu.edu>
2026-05-20 14:55:19 -0700

    Ch6 Update

--- Chapter06/Fisher_ss_pp.py
+++ Chapter06/Fisher_ss_pp.py
@@ -2,10 +2,8 @@
 """
 Phase Plane for the Steady State of the Fisher Equation
 
-Note: Here we've leveraged the first integral to identify trajectories in the
-case of Dirichlet boundary conditions, but this might change if the BCs are
-Robin instead.  In the latter case we may need to approximate the solution of
-the DE numerically.
+We find a numerical solution of the associated ODE, leveraging the event
+switch in solve_ivp to stop integrating when needed.
 
 """
 
@@ -19,7 +17,7 @@ from scipy.integrate import solve_ivp
 # =============================================================================
 # DE RHS
 # =============================================================================
-def de_rhs(t, y):
+def de_rhs(t, y, delta):
 	v, w = y
 	dv = w
 	dw = -v * (1 - v)
@@ -27,60 +25,88 @@ def de_rhs(t, y):
 	return dy
 
 # =============================================================================
-# Solution Trajectory
+# Dirichlet Boundary Conditions Event and Event Attributes
 # =============================================================================
-def Ysoln(tt, Uinit):
-	tf = tt[-1]
-	soln = solve_ivp(de_rhs, [0, tf], Uinit, dense_output=True)
-	Y = soln.sol(tt).T
-	V = Y[:, 0]
-	W = Y[:, 1]
-	return V, W
-# =============================================================================
-# First Integral
-# =============================================================================
-def F(v, w):
-	z = 1/2*w**2 + 1/2*v**2 - 1/3*v**3
+"""
+The Dirichlet boundary conditions on the PDE give rise to a phase plane with
+v > 0 in traveling wave coordinates.  We ensure that the IVP solver returns 
+such a solution.
+"""
+def D_BC_zero(t, y, delta):
+	v, w = y
+	z = v
 	return z
 
+D_BC_zero.direction = -1	# Ensure v is decreasing 
+D_BC_zero.terminal = True	# Trigger at first zero when z is decreasing
+
+# =============================================================================
+# Robin Boundary Conditions Event and Event Attributes
+# =============================================================================
+"""
+The Robin boundary conditions on the PDE give rise to a phase plane with
+v > |w/delta| in traveling wave coordinates.  We ensure that the IVP solver returns 
+such a solution.
+"""
+def R_BC_zero(t, y, delta):
+	v, w = y
+	z = delta * v + w
+	return z
+
+R_BC_zero.direction = -1	# Ensure v is decreasing 
+R_BC_zero.terminal = True	# Trigger at first zero when z is decreasing
 
 # =============================================================================
 # Main Simulation Function
 # =============================================================================
 def Fisher_ss_pp():
+	tmax = 20
+	D = 0.1
+	delta = 1/D 
 
-	fig, ax = plt.subplots()
-	ax.plot([0, 1], [0, 0], 'ok')
-	ax.set(xlabel = 'v', ylabel = 'w')
+	# --- Dirichlet Boundary Conditions
+	fig1, ax1 = plt.subplots()
+	ax1.plot([0, 1], [0, 0], 'ok')
+	ax1.set(xlabel = 'v', ylabel = 'w')
 
-
-
-	v = np.linspace(0, 1, 2**8+1)
-	w = np.linspace(-0.6, 0.6, 2**8+1)
-	V, W = np.meshgrid(v, w)
-	
-	w0_max = np.sqrt(1/3)
+	w0_max = np.sqrt(1/3)+2e-4
 	w0_list = np.linspace(0.1, w0_max, 5)
-	levels = 1/2*w0_list**2
-	levels = np.sort(levels)
-	
-	ax.contour(V, W, F(V, W), levels)#, colors=['black'])
-	
-	plt.show()
-
-	"""
-	w0_list = np.linspace(0.1, 0.5, 5)
-	t = np.linspace(0, 10, 2**8+1)
-	fig, ax = plt.subplots()
-	ax.plot([0, 1], [0, 0], 'ok')
 	for jw0 in range(len(w0_list)):
 		w0 = w0_list[jw0]
 		Y_init = np.array([0, w0])
-		v, w = Ysoln(t, Y_init)
-		ax.plot(v, w, 'k')
-	ax.set(xlim=(0, 1), ylim=(-0.5, 0.5))
+		soln = solve_ivp(de_rhs, [0, tmax], Y_init, args=[delta],
+						events = D_BC_zero, dense_output = True)
+		tf = max(soln.t)
+		t = np.linspace(0, tf, 2**8+1)
+		y = soln.sol(t)
+		v = y[0,:]
+		w = y[1,:]
+		ax1.plot(v, w)
+	ax1.plot([0, 0], [-w0_max, w0_max], '--r')
+	
+	# --- Robin Boundary Conditions
+
+	fig2, ax2 = plt.subplots()
+	ax2.plot([0, 1], [0, 0], 'ok')
+	ax2.set(xlabel = 'v', ylabel = 'w')
+
+	w0_max = 0.567125
+	ax2.plot([0, w0_max/delta], [0, w0_max], '--r')
+	ax2.plot([0, w0_max/delta], [0, -w0_max], '--r')
+	w0_list = np.linspace(0.1, w0_max, 5)
+	for jw0 in range(len(w0_list)):
+		w0 = w0_list[jw0]
+		Y_init = np.array([w0/delta, w0])
+		soln = solve_ivp(de_rhs, [0, tmax], Y_init, args = [delta],
+						events = R_BC_zero,  dense_output = True)
+		tf = max(soln.t)
+		t = np.linspace(0, tf, 2**8+1)
+		y = soln.sol(t)
+		v = y[0,:]
+		w = y[1,:]
+		ax2.plot(v, w)
+
 	plt.show()
-	"""
 
 # =============================================================================
 # Execute the simulation if the script is run directly
