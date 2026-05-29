88b3037c9b1a54e0260c88ffa4f11c7eb4d17017
Frank Lynch <lynch@ewu.edu>
2026-05-29 16:11:24 -0700

    Ch5 Update

---
+++ Chapter05/MOL_Fisher_NR.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Fisher Equation with Neumann or Robin conditions via Method of Lines
+
+"""
+
+# =============================================================================
+# Packages
+# =============================================================================
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+import matplotlib.animation as manimation
+rng = np.random.default_rng()
+
+# =============================================================================
+# Main Simulation Function
+# =============================================================================
+def MOL_Fisher_NR():
+
+
+
+# =============================================================================
+# Execute the simulation if the script is run directly
+# =============================================================================
+if __name__ == "__main__":
+    MOL_Fisher_NR()
