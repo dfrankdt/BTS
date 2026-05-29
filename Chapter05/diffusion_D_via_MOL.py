3c4e9bb1b9da382f920c845a29e4c2d84615a755
dfrankdt <lynch@ewu.edu>
2026-05-29 14:57:52 -0700

    Ch5 Update

--- Chapter05/diffusion_D_via_MOL.py
+++ Chapter05/diffusion_D_via_MOL.py
@@ -3,7 +3,12 @@
 Diffusion with Dirichlet BCs via the Method of Lines
 
 We approximate the solution of the Diffusion Equation (5.37) -- (5.38) with
-Dirichlet boundary conditions using the method of lines
+Dirichlet boundary conditions using the method of lines. We discretize the
+spatial region into N subintervals (N+1 endpoints) and consisder the N-1
+interior points.
+
+With zero Dirichlet conditions, the boundary points are fixed and we need only
+solve the IVP on the interior points.
 
 """
 
@@ -18,40 +23,77 @@ import matplotlib.animation as manimation
 # =============================================================================
 # DE RHS
 # =============================================================================
-def de_rhs(t, u, alpha):
-	Nu = np.size(u)
-	sc = 2*np.ones(Nu)
-	sc[0] = 1
-	sc[-1] = 1
-	sp = np.zeros(Nu)
-	sp[1:] = u[:Nu-1]
-	sm = np.zeros(Nu)
-	sm[:-1] = u[1:Nu]
-	d2u = -sc*u + sp + sm
-	du = alpha*( d2u )
-	return du
-	
+def de_rhs(t, u, alpha, u0, uL):
+        N = len(u)-1
+
+        d2u = np.zeros(N+1)
+        d2u[1:N] = u[0:N-1] - 2*u[1:N] + u[2:N+1]
+        d2u[0] = u0 - 2*u[0] + u[1]
+        d2u[-1] = u[-2] - 2*u[-1] + uL
+        du = alpha*d2u
+        return du
+
+# =============================================================================
+# Animation
+# =============================================================================
+def doMovie(x, t, U):
+        # --- Initialize data structures
+        Nt = len(t) - 1
+        uinit = U[:,0]
+
+        # --- Initialize movie
+        fig, ax = plt.subplots()
+        p_init = ax.plot(x, uinit, '-r', label='Initial Profile')
+        p_update = ax.plot([], [], '-b', label='Time Evolution')[0]
+        ax.set(xlabel='x', ylabel='u(x, t)')
+        ax.legend(loc='upper right')
+
+        # --- Function to update the plot with the current frame
+        def update(frame):
+                tk = t[frame]
+                u = U[:, frame]
+                p_update.set_xdata(x)
+                p_update.set_ydata(u)
+                ax.set(title=f'Time t = {tk:.2f} s')
+                return(p_update)
+
+        ani = manimation.FuncAnimation(fig=fig, func=update, frames=range(Nt + 1), interval=100)
+        plt.show()
 
 # =============================================================================
 # Main Simulation Function
 # =============================================================================
 def diffusion_D_via_MOL():
-	# --- Global parameters
-	D = 1			# diffusion coefficient
-	u0, uL = 0, 0	# boundary conditions
-	L = 1			# length of domain
-	Nx = 2**5		# number of partitions in x
-	tf = 0.5		# end time
-	dt = 0.01		# time step
-	
-	# --- Variables
-	x = np.linspace(0, L, Nx + 1)
-	dx = L/Nx
-	alpha = D/dx**2
-	Nt = int(dt/tf)
+        # --- Global parameters
+        D = 1			# diffusion coefficient
+        L = 1			# length of domain
+        Nx = 2**6		# number of partitions in x
+        tf = 0.5		# end time
+        dt = 0.01		# time step
+        u0, uL = 0, 0	        # boundary conditions
+
+        # --- Variables
+        x = np.linspace(0, L, Nx + 1)
+        dx = L/Nx
+        Nt = int(tf/dt)
+        t = np.linspace(0, tf, Nt + 1)
+        alpha = D/dx**2
+        U = np.zeros( (Nx+1, Nt+1) )
+
+        # --- Initial profile (Gaussian)
+        x_interior = x[1:Nx]
+        uinit = np.exp(- (x_interior - L/2)**2/(2*dx) )
+
+        # --- Solve the IVP on the interior points
+        soln = solve_ivp(de_rhs, [0, tf], uinit, args=[alpha, u0, uL], dense_output=True)
+        U[1:Nx,:] = soln.sol(t)
+
+        # --- Animation
+        doMovie(x, t, U)
+        
 
 # =============================================================================
 # Execute the simulation if the script is run directly
 # =============================================================================
 if __name__ == "__main__":
-    diffusion_D_via_MOL()
+        diffusion_D_via_MOL()
--- Chapter05/diffusion_NR_via_MOL.py
+++ Chapter05/diffusion_NR_via_MOL.py
@@ -14,12 +14,88 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 import matplotlib.animation as manimation
-rng = np.random.default_rng()
+
+
+# =============================================================================
+# DE RHS -- Neumann or Robin Condition
+# =============================================================================
+def de_rhs_NR(t, u, alpha, R, B):
+        N = len(u)-1
+        R0, RN = R
+        B0, BN = B
+
+        d2u = np.zeros(N+1)
+        d2u[1:N] = u[0:N-1] - 2*u[1:N] + u[2:N+1]
+        d2u[0] = (-2 - R0)*u[0] + 2*u[1] + B0
+        d2u[-1] = 2*u[-2] + (-2 - RN)*u[-1] + BN
+        du = alpha*d2u
+        return du
+
+# =============================================================================
+# Animation
+# =============================================================================
+def doMovie(x, t, U):
+        # --- Initialize data structures
+        Nt = len(t) - 1
+        uinit = U[:,0]
+
+        # --- Initialize movie
+        fig, ax = plt.subplots()
+        p_init = ax.plot(x, uinit, '-r', label='Initial Profile')
+        p_update = ax.plot([], [], '-b', label='Time Evolution')[0]
+        ax.set(xlabel='x', ylabel='u(x, t)')
+        ax.legend(loc='upper right')
+
+        # --- Function to update the plot with the current frame
+        def update(frame):
+                tk = t[frame]
+                u = U[:, frame]
+                p_update.set_xdata(x)
+                p_update.set_ydata(u)
+                ax.set(title=f'Time t = {tk:.2f} s')
+                return(p_update)
+
+        ani = manimation.FuncAnimation(fig=fig, func=update, frames=range(Nt + 1), interval=100)
+        plt.show()
+        
+
 
 # =============================================================================
 # Main Simulation Function
 # =============================================================================
 def diffusion_NR_via_MOL():
+        # --- Global parameters
+        D = 1			# diffusion coefficient
+        L = 1			# length of domain
+        Nx = 2**6		# number of partitions in x
+        tf = 0.5		# end time
+        dt = 0.01		# time step
+
+        # --- Boundary Conditions (zero porosity for Neumann condition)
+        u0, uL = 0.8, .1        # value at boundary, outside for Robin condition
+        del0, delL = 1, 1       # porosity (reaction rate) for Robin condition
+
+        # --- Variables
+        x = np.linspace(0, L, Nx + 1)
+        dx = L/Nx
+        Nt = int(tf/dt)
+        t = np.linspace(0, tf, Nt + 1)
+        alpha = D/dx**2
+        U = np.zeros( (Nx+1, Nt+1) )
+
+        # --- Initial profile (Gaussian)
+        uinit = np.exp(- (x - L/2)**2/(2*dx) )
+
+        # --- Boundary conditions to pass to solver
+        R = (2*dx/D) * np.array([del0, delL])
+        B = (2*dx/D) * np.array([del0*u0, delL*uL])
+
+        # --- Solve the IVP on the interior points
+        soln = solve_ivp(de_rhs_NR, [0, tf], uinit, args=[alpha, R, B], dense_output=True)
+        U = soln.sol(t)
+
+        # --- Animation
+        doMovie(x, t, U)
 
 
 
