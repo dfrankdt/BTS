e6e22075014b3d249a540d0d6dd636e5b5b19ede
Frank Lynch <lynch@ewu.edu>
2026-06-11 20:23:51 -0700

    Ch5 Update

--- Chapter05/CN_diffusion_NR.py
+++ Chapter05/CN_diffusion_NR.py
@@ -9,16 +9,22 @@ Diffusion with Neumann or Robin BCs via Crank-Nicolson
 # =============================================================================
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import solve_ivp
 import matplotlib.animation as manimation
-rng = np.random.default_rng()
 
 # =============================================================================
 # Main Simulation Function
 # =============================================================================
 def CN_Diffusion_NR():
-
-
+	# --- Parameters
+	L = 1
+	D = 1
+	
+	# --- Discretizations
+	Nt, Nx = 2**4, 2**5
+	dt, dx = 	
+	# --- Boundary Conditions
+	del0, U0 = 0, 0	# For Robin -Du_x + del0 u0 = U0
+	delL, UL = 0, 0 # For Robin  Du_x + delL uN = UL
 
 # =============================================================================
 # Execute the simulation if the script is run directly
