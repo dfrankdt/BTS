0e17cd3507b0f223697f0534f84a0292b61718f0
dfrankdt <lynch@ewu.edu>
2026-06-23 11:44:58 -0700

    Update

---
+++ Playground/D2_radial_test.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+"""
+Second difference operator leveraging radial symmetry
+
+I'd like to be sure that the second difference operator in polar coordinates
+with radial symmetry is correct.  So I'll construct such an operator and test
+it on the function
+
+ u(r, t) = exp(-kt) * exp(-r^2)
+ 
+for which the second derivative in space satisfies
+
+  nabla^2 u = 1/r d/dr ( r du/dr ) = 4(r^2 - 1) * exp(-kt) * exp(-r^2)
+
+"""
+
+# =============================================================================
+# Packages
+# =============================================================================
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as manimation
+
+
+# =============================================================================
+# Second Difference Operator
+# =============================================================================
+def D2_polar(r, u):
+	dr = r[1] - r[0]
+	Nr = len(r) - 1
+	
+	# --- Interior discretizations
+	rp = (r[2:Nr+1] + r[1:Nr])/2
+	rm = (r[1:Nr] + r[0:Nr-1])/2
+
+	# --- Second derivative operator
+	D2 = -2*np.eye(Nr+1)
+	D2p = np.zeros(Nr)
+	D2p[1:Nr] = rp/r[1:Nr]
+	D2m = np.zeros(Nr)
+	D2m[0:Nr-1] = rm/r[1:Nr]
+	D2 = D2 + np.diag( D2p, k=1) + np.diag(D2m, k=-1)
+	print(D2)
+	# --- Adjust at boundaries
+	D2u = 1/dr**2 * (D2@u)
+	return D2u
+
+# =============================================================================
+# Main Simulation Function
+# =============================================================================
+def D2_radial_test():
+	# --- Parameters
+	k = 0.1
+	R = 2
+	Nr = 2**3
+	r = np.linspace(0, R, Nr+1)
+	
+	u = np.exp(-r**2)
+	upp = D2_polar(r, u)
+
+	fig, (ax1, ax2) = plt.subplots(1, 2, figsize= (12.8, 4.8) )
+	ax1.plot(r[1:Nr], upp[1:Nr])
+	ax1.plot(r[1:Nr], 4*(r[1:Nr]**2 - 1)*u[1:Nr], '--r')
+	ax2.plot(r[1:Nr], upp[1:Nr] - 4*(r[1:Nr]**2 - 1)*u[1:Nr])
+	plt.show()
+	
+		
+
+
+# =============================================================================
+# Execute the simulation if the script is run directly
+# =============================================================================
+if __name__ == "__main__":
+    D2_radial_test()
