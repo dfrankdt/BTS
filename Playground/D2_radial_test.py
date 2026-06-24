69f829246b6671ed63673a6aa95c949787108c8b
dfrankdt <lynch@ewu.edu>
2026-06-24 12:55:08 -0700

    Update

--- Playground/D2_radial_test.py
+++ Playground/D2_radial_test.py
@@ -6,11 +6,11 @@ I'd like to be sure that the second difference operator in polar coordinates
 with radial symmetry is correct.  So I'll construct such an operator and test
 it on the function
 
- u(r, t) = exp(-kt) * exp(-r^2)
+ u(r, t) = exp(-r^2)
  
 for which the second derivative in space satisfies
 
-  nabla^2 u = 1/r d/dr ( r du/dr ) = 4(r^2 - 1) * exp(-kt) * exp(-r^2)
+  nabla^2 u = 1/r d/dr ( r du/dr ) = 4(r^2 - 1) * exp(-r^2)
 
 """
 
@@ -34,15 +34,16 @@ def D2_polar(r, u):
 	rm = (r[1:Nr] + r[0:Nr-1])/2
 
 	# --- Second derivative operator
-	D2 = -2*np.eye(Nr+1)
-	D2p = np.zeros(Nr)
-	D2p[1:Nr] = rp/r[1:Nr]
-	D2m = np.zeros(Nr)
-	D2m[0:Nr-1] = rm/r[1:Nr]
+	D2 = -2*np.eye(Nr-1)
+	D2p = rp[0:Nr-2]/r[1:Nr-1]
+	D2m = rm[1:Nr-1]/r[2:Nr]
 	D2 = D2 + np.diag( D2p, k=1) + np.diag(D2m, k=-1)
-	print(D2)
+
 	# --- Adjust at boundaries
-	D2u = 1/dr**2 * (D2@u)
+#	D2[0, 0] = D2[0, 0] + rm[0]/r[1]
+	D2u = 1/dr**2 * (D2@u[1:Nr])
+	D2u[-1] = D2u[-1] + 1/dr**2 * (rp[-1]/r[-2]*u[-1])
+	D2u[0] = D2u[0] + 1/dr**2 * (rm[0]/r[1]*u[0])
 	return D2u
 
 # =============================================================================
@@ -50,22 +51,18 @@ def D2_polar(r, u):
 # =============================================================================
 def D2_radial_test():
 	# --- Parameters
-	k = 0.1
 	R = 2
-	Nr = 2**3
+	Nr = 2**9
 	r = np.linspace(0, R, Nr+1)
 	
 	u = np.exp(-r**2)
 	upp = D2_polar(r, u)
 
 	fig, (ax1, ax2) = plt.subplots(1, 2, figsize= (12.8, 4.8) )
-	ax1.plot(r[1:Nr], upp[1:Nr])
+	ax1.plot(r[1:Nr], upp)
 	ax1.plot(r[1:Nr], 4*(r[1:Nr]**2 - 1)*u[1:Nr], '--r')
-	ax2.plot(r[1:Nr], upp[1:Nr] - 4*(r[1:Nr]**2 - 1)*u[1:Nr])
+	ax2.plot(r[1:Nr], upp - 4*(r[1:Nr]**2 - 1)*u[1:Nr])
 	plt.show()
-	
-		
-
 
 # =============================================================================
 # Execute the simulation if the script is run directly
