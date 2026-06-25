0c02da7f3b88a291c387b851b6d68813979e5617
dfrankdt <lynch@ewu.edu>
2026-06-24 17:29:04 -0700

    Ch7 Update

--- Chapter07/calcium_CICR_rhs.py
+++ Chapter07/calcium_CICR_rhs.py
@@ -24,9 +24,33 @@ def calcium_CICR_rhs():
 	K2 = 0.08
 	gm = 5.5
 	Kp = 0.1
+	
+	# --- Flux functions
+	def Jserca(c):
+		z = Vp*c**2/(Kp**2 + c**2)
+		return z
+	
+	def JIPR(c):
+		P = p**3/(p + K1)**3
+		P0 = P*c**3/(c + K2)**3
+		ce = gm*(ct - c)
+		z = kf*P0*(ce - c)
+		return z
 
+	# --- Varying parameters	
 	p_list = np.array([0.12, 0.15, 0.2])
-
+	c = np.linspace(0, ct, 2**6+1)
+	
+	fig, ax = plt.subplots()
+	ax.plot(c, np.zeros(len(c)), '--k')
+	for kp in range(len(p_list)):
+		p = p_list[kp]
+		Jtot = 	JIPR(c) - Jserca(c)
+		ax.plot(c, Jtot, label=f'p = {p:.2f}')
+	ax.set(xlabel=r'Ca$^{++}$ ($\mu$M)', ylabel = r'$J_\text{total}$')
+	ax.set(ylim=(-0.8, 0.8))
+	ax.legend()
+	plt.show()
 
 # =============================================================================
 # Execute the simulation if the script is run directly
