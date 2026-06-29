afcf422b35fa34d12cbaa71ae7c34f881c5b9d6d
dfrankdt <lynch@ewu.edu>
2026-06-29 09:42:19 -0700

    Ch8 Update

--- Chapter08/bistable_threshold_2D.py
+++ Chapter08/bistable_threshold_2D.py
@@ -28,17 +28,18 @@ def F(u, alpha):
 # DE RHS
 # =============================================================================
 def de_rhs(xi, y, alpha):
-	u, w = y
+	u, w, v = y
 	du = -w/xi
 	dw = xi*F(u, alpha)
-	dy = np.array([du, dw])
+	dv = u*xi
+	dy = np.array([du, dw, dv])
 	return dy
 
 # =============================================================================
 # DE Events
 # =============================================================================
 def de_event(t, y, alpha):
-	u, w = y
+	u, w, v = y
 	z = u*w
 	return z
 de_event.terminal = 1
@@ -50,7 +51,7 @@ de_event.direction = -1
 def bistable_threshold_2D():
 	a_list = [0.23, 0.2438355, 0.25]
 	alpha = 0.1
-	ximax = 100
+	ximax = 1000
 	
 	IVP_args = [alpha]
 	fig1, ax1 = plt.subplots()
@@ -58,24 +59,27 @@ def bistable_threshold_2D():
 	fig2, ax2 = plt.subplots()
 	ax2.set(xlabel=r'$\xi$', ylabel = r'u($\xi$)')
 	
-	for kc in range(len(a_list)):
-		a = a_list[kc]
-		xi0 = 0.01
+	for ka in range(len(a_list)):
+		a = a_list[ka]
+		xi0 = 1e-6
 		u0 = a - F(a, alpha)/4*xi0**2
 		w0 = F(a, alpha)/2*xi0**2
 		
-		y0 = [u0, w0]
+		y0 = [u0, w0, 0]
 		soln = solve_ivp(de_rhs, [xi0, ximax], y0, args=IVP_args,
 			events=de_event, dense_output=True)
 		xif = max(soln.t)
 		xi = np.linspace(xi0, xif, 2**8+1)
-		U, W = soln.sol(xi)
+		U, W, IU = soln.sol(xi)
 		ax1.plot(U, W, label=f'a = {a:1.5f}')
 		ax2.plot(xi, U, label=f'a = {a:1.5f}')
+		if ka==1:
+			print(f'The value of the definite integral is {2*np.pi*IU[-1]:1.4f}')
+
 	ax1.legend(loc='upper right')
 	ax2.legend(loc='upper right')
+
 	plt.show()
-	
 		
 
 
