59e3c933a873cd29308a73f3bc1363c683b3ff33
dfrankdt <lynch@ewu.edu>
2026-09-15 11:48:29 -0700

    Ch8 Update

--- Chapter08/barrier_block.py
+++ Chapter08/barrier_block.py
@@ -7,9 +7,8 @@ Figures produced:
  - Figure 1: Trajectory (U-W plane), see Figure 8.6(a)
  - Figure 2: Trajectory (xi-U plane), see Figure 8.6(b)
  - Figure 3: Length of curve Y as a function of U(0), see Figure 8.7(a)
- - Figure 4: Critical blocking
+ - Figure 4: Critical blocking, see Figure 8.7(b)
 
-TO DO: Definitely getting lost in the sauce on this one
 """
 
 # =============================================================================
