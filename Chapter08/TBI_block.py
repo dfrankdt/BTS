806750ac6f1d9fe9b72824ea59c010b01a081f0d
Frank Lynch <lynch@ewu.edu>
2026-09-17 19:17:46 -0700

    Ch8 Update

--- Chapter08/TBI_block.py
+++ Chapter08/TBI_block.py
@@ -14,10 +14,25 @@ import matplotlib.animation as manimation
 rng = np.random.default_rng()
 
 # =============================================================================
+# Function Definitions
+# =============================================================================
+def f(x, zeros):
+	# --- Cubic nonlinearity
+	x0, x1, x2 = zeros
+	y = (x-x0)*(x1-x)*(x-x2)
+	return y
+
+def F(x, a):
+	# --- Integral of the cubic nonlinearity
+	y = -(x**4/4 - (a+1)/3*x**3 + a/2*x**2)
+	return y
+
+# =============================================================================
 # Main Simulation Function
 # =============================================================================
 def TBI_block():
-
+	# --- Parameters
+	Ar = 
 
 
 # =============================================================================
