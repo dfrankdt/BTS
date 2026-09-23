6b7aa18412de5f68cebe328e4398f290b9afdcee
Frank Lynch <lynch@ewu.edu>
2026-09-23 08:43:49 -0700

    Ch8 Update

--- Chapter08/fdf_plots.py
+++ Chapter08/fdf_plots.py
@@ -9,34 +9,46 @@ Fire-Diffuse-Fire Model:
 # =============================================================================
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import solve_ivp
-import matplotlib.animation as manimation
-rng = np.random.default_rng()
 
 # =============================================================================
 # Main Simulation Function
 # =============================================================================
 def fdf_plots():
-	# --- Parameters
-	n = 1000	# large
-	dt = np.linspace(20/2**8, 20, 2**8)
-	blist = np.array([0.25, 0.05, 0.01])
-	colorlist = np.array(['b', 'g', 'y'])
-	
-	fig, ax = plt.subplots()
+	# --- Parameters: For this simulation a logarithmically spacing is appropriate
+	n = 1000
+	dt = np.logspace(np.log(25/2**8)/np.log(10), np.log(25)/np.log(10), 2**8)
+	blist = np.array([0.01, 0.05, 0.25])
+
+	# --- Initialize Plot
+	fig, (ax1, ax2) = plt.subplots(1, 2, figsize= (12.8, 4.8) )
+	colorlist = np.array(['b', 'g', 'r'])
+
+	# --- Step through beta values	
 	for kb in range(len(blist)):
 		b = blist[kb]
-		c = colorlist[kb]
 		theta = 0
 		for kn in range(1, n+1):
 			dtheta = 1/np.sqrt(4*np.pi*kn*dt)*np.exp(-kn/(4*dt) - b**2*kn*dt)
 			theta += dtheta
+		# --- Capture maximum value: we are interested in the increasing curve
 		knmax = np.argmax(theta)
-		ax.plot(dt[:knmax], theta[:knmax], color=[c],
-				dt[knmax:], theta[knmax:], )
-	plt.show()
+		
+		# --- Theta as a function of dt
+		ax1.plot(dt[:knmax], theta[:knmax], '-', color = colorlist[kb],
+			label = rf'$\beta = ${b:1.2f}')
+		ax2.plot(theta[:knmax], 1/dt[:knmax], '-', color = colorlist[kb], 
+			label = rf'$\beta = ${b:1.2f}')
 			
-	
+		# --- 1/dt as a function of Theta
+		ax1.plot(dt[knmax:], theta[knmax:], '--', color = colorlist[kb])
+		ax2.plot(theta[knmax:], 1/dt[knmax:], '--', color = colorlist[kb])
+
+	# --- Labeling
+	ax1.set(xlabel = r'$\delta_\tau$', ylabel = r'$\theta = g_\beta(\delta_\tau)$')
+	ax2.set(xlabel = r'$\theta = c^*L/\sigma$', ylabel = r'$1/\delta_\tau$')
+	ax1.legend(loc = 'upper right')
+	ax2.legend(loc = 'upper right')
+	plt.show()
 
 # =============================================================================
 # Execute the simulation if the script is run directly
